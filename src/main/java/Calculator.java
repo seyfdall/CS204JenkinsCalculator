98703ddc99cc250ab5d1139483d9c5d6141426b6
Dallin Seyfried <dseyfr99@gmail.com>
2022-01-11 11:49:52 -0700

    Finishing up all unit test problems

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -18,6 +19,9 @@ class Calculator {
     }
 
     int divide(int a , int b){
+        if (b == 0) {
+            throw new ArithmeticException("/ by zero");
+        }
         return a / b;
     }
 
@@ -75,8 +79,8 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        UUID uuid = UUID.randomUUID();
+        return n + uuid.toString();
     }
 
 
